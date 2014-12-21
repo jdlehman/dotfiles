3cff2fe8cbf1f0806800bd938c23b75e1b014e26
Jonathan Lehman <jonathan.lehman91@gmail.com>
2014-12-20 20:17:54 -0500

    emacs: Formatting for readability

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,17 +21,18 @@
 (add-to-list 'load-path jl/core-dir)
 (add-to-list 'load-path jl/modules-dir)
 
-;; Package Manager
-;; See Cask file for its configuration
-;; https://github.com/cask/cask
-(defvar cask-location (car (file-expand-wildcards "/usr/local/Cellar/cask/*/cask.el")))
-(require 'cask cask-location)
-(cask-initialize)
+;;; Package Manager {{{
+  ;; See Cask file for its configuration
+  ;; https://github.com/cask/cask
+  (defvar cask-location (car (file-expand-wildcards "/usr/local/Cellar/cask/*/cask.el")))
+  (require 'cask cask-location)
+  (cask-initialize)
 
-;; Keeps Cask file in sync with the packages
-;; that you install/uninstall via ~M-x list-packages~
-;; https://github.com/rdallasgray/pallet
-(require 'pallet)
+  ;; Keeps Cask file in sync with the packages
+  ;; that you install/uninstall via ~M-x list-packages~
+  ;; https://github.com/rdallasgray/pallet
+  (require 'pallet)
+;;; }}}
 
 ;; require core
 (require 'jl-ui)
