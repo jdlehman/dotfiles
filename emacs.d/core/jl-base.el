f4842f6f7c6d7af592b21deffd87ad5676400be7
Jonathan Lehman <jonathan.lehman91@gmail.com>
2014-12-23 10:23:20 -0500

    emacs: Visual bell

--- emacs.d/core/jl-base.el
+++ emacs.d/core/jl-base.el
@@ -50,14 +50,17 @@
 ;; instantly display current keystrokes in mini buffer
 (setq echo-keystrokes 0.1)
 
+;; delete the selection area with a keypress
+; (delete-selection-mode t)
+
+;; visible bell
+(setq visible-bell t)
+
 ;;; keybindings {{{
   ;; auto-indent on RET
   (define-key global-map (kbd "RET") 'newline-and-indent)
   ;; remove ESC keybindings (use Meta instead). now equivalent to C-g
   (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 ;;; }}}
-
-;; delete the selection area with a keypress
-; (delete-selection-mode t)
 
 (provide 'jl-base)
