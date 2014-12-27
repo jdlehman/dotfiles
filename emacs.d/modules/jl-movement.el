5c7b10961dc10cb1902d4cf49ecfbf16cabda6b1
Jonathan Lehman <jonathan.lehman91@gmail.com>
2014-12-26 19:45:02 -0500

    emacs: Add movement module
    - Move to beginning of line or first non-blank character (C-a)
    - Remap (C-x l) to go to line number

---
+++ emacs.d/modules/jl-movement.el
@@ -0,0 +1,25 @@
+;;; FUNCTIONS {{{
+  ;; from http://vickychijwani.me/nuggets-from-my-emacs-part-i/
+  ;; "smart" home, i.e., home toggles b/w 1st non-blank character and 1st column
+  (defun jl/smart-beginning-of-line ()
+    "Move point to first non-whitespace character or beginning-of-line."
+    (interactive "^") ; Use (interactive "^") in Emacs 23 to make shift-select work
+    (let ((oldpos (point)))
+      (back-to-indentation)
+      (and (= oldpos (point))
+           (beginning-of-line))))
+;;; }}}
+
+;;; SETTINGS {{{
+;;; }}}
+
+;;; KEYBINDINGS {{{
+  ;; smart beginning of line
+  (global-set-key [home] 'jl/smart-beginning-of-line)
+  (global-set-key (kbd "C-a") 'jl/smart-beginning-of-line)
+
+  ;; go to line
+  (global-set-key (kbd "C-x l") 'goto-line)
+;;; }}}
+
+(provide 'jl-movement)
