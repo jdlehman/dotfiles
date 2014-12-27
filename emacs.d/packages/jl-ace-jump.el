f89d5ada6c501343807360cf8b7ee4dfc902e6e3
Jonathan Lehman <jonathan.lehman91@gmail.com>
2014-12-27 12:22:30 -0500

    emacs: Add ace-window package
    - Move between windows quickly

--- emacs.d/packages/jl-ace-jump.el
+++ emacs.d/packages/jl-ace-jump.el
@@ -1,6 +1,11 @@
 (require 'ace-jump-mode)
 (require 'ace-jump-buffer)
+(require 'ace-window)
 
 (global-set-key (kbd "C-c SPC") 'ace-jump-mode)
+(global-set-key (kbd "C-c w") 'ace-window)
+
+;; set ace window keys to be home keys
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 
 (provide 'jl-ace-jump)
