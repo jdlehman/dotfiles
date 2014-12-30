c7ebfcad7118eb393bbb39cfb1bdafeb3c31c49c
Jonathan Lehman <jonathan.lehman91@gmail.com>
2014-12-30 13:49:11 -0500

    emacs: Update web-mode settings
    - Set indentation for script/style tags
    - Auto pair
    - Highlight current tag

--- emacs.d/packages/jl-web-mode.el
+++ emacs.d/packages/jl-web-mode.el
@@ -6,4 +6,10 @@
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
 
+(setq web-mode-script-padding 2)
+(setq web-mode-style-padding 2)
+
+(setq web-mode-enable-auto-pairing t)
+(setq web-mode-enable-current-element-highlight t)
+
 (provide 'jl-web-mode)
