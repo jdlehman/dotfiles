5b6b5495d5dc462e521702a02529bc4107e40837
Jonathan Lehman <jonathan.lehman91@gmail.com>
2015-01-02 11:41:22 -0500

    emacs: Update visual regexp mappings

--- emacs.d/packages/jl-visual-regexp.el
+++ emacs.d/packages/jl-visual-regexp.el
@@ -1,10 +1,10 @@
 (require 'visual-regexp)
 
 ;; replace all occurrences
-(define-key global-map (kbd "C-c v r") 'vr/replace)
+(define-key global-map (kbd "C-c r") 'vr/replace)
 ;; ask for eaach occurrence
-(define-key global-map (kbd "C-c v q") 'vr/query-replace)
+(define-key global-map (kbd "C-c q") 'vr/query-replace)
 ;; mark multiple-cursors based on regex
-(define-key global-map (kbd "C-c v m") 'vr/mc-mark)
+(define-key global-map (kbd "C-c m") 'vr/mc-mark)
 
 (provide 'jl-visual-regexp)
