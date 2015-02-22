65f0180ee48b09385ab3b2ca57660c2b6f9b2028
Jonathan Lehman <jonathan.lehman91@gmail.com>
2015-02-22 17:57:42 -0500

    emacs: occur with sane default

--- emacs.d/modules/jl-misc.el
+++ emacs.d/modules/jl-misc.el
@@ -3,12 +3,25 @@
 ;; a module of their own
 
 ;;; FUNCTIONS {{{
+  ;; occur-do what i mean
+  ;; http://oremacs.com/2015/01/26/occur-dwim/
+  (defun occur-dwim ()
+    "Call `occur' with a sane default."
+    (interactive)
+    (push (if (region-active-p)
+            (buffer-substring-no-properties
+              (region-beginning)
+              (region-end))
+            (thing-at-point 'symbol))
+          regexp-history)
+    (call-interactively 'occur))
 ;;; }}}
 
 ;;; SETTINGS {{{
 ;;; }}}
 
 ;;; KEYBINDINGS {{{
+  (global-set-key (kbd "M-s o") #'occur-dwim)
 ;;; }}}
 
 ;;; EVIL MAPPINGS {{{
