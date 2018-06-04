6d3e72fc6d3faaca7929bcbd777a1385e1084d9c
Jonathan Lehman <jonathan.lehman91@gmail.com>
2018-06-04 09:41:45 -0400

    zsh: Remove hardcoded dir

---
+++ zsh/zplug.zsh
@@ -0,0 +1,17 @@
+# #set up zplug
+# export ZPLUG_HOME=/usr/local/opt/zplug
+# source $ZPLUG_HOME/init.zsh
+
+# zplug "plugins/docker", from:oh-my-zsh
+# zplug "plugins/docker-compose", from:oh-my-zsh
+
+# # Install plugins if there are plugins that have not been installed
+# if ! zplug check --verbose; then
+#     printf "Install? [y/N]: "
+#     if read -q; then
+#         echo; zplug install
+#     fi
+# fi
+
+# # Then, source plugins and add commands to $PATH
+# zplug load
